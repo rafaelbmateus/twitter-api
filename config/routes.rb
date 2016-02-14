8477a8ecda5b9c4124d28e0d6a4273fb061c231c
Rafael Mateus <rafaelmateus@hotmail.com.br>
2016-02-14 00:21:44 -0200

    Fix version routes users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, except: [:new, :edit]
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :users, except: [:new, :edit]
       resources :tweets, except: [:new, :edit]
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214011313) do
+ActiveRecord::Schema.define(version: 20160214021428) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
